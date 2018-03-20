1be5a677a03d0d7ff2b974bfd5407739aa3556e4
16anitta <100306951@alumnos.uc3m.es>
2018-03-20 15:30:19 -0400

    Notification method does not exist anymore

--- app/src/main/java/com/codecaptured/autoagenda/MainActivity.java
+++ app/src/main/java/com/codecaptured/autoagenda/MainActivity.java
@@ -6,22 +6,11 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Button;
-import android.view.View;
-import android.content.Intent;
-import android.app.PendingIntent;
-import android.app.NotificationManager;
-import android.support.v4.app.NotificationCompat;
-import android.content.Context;
-import android.os.Build;
-import android.app.NotificationChannel;
-import android.app.Notification;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-	  private NotificationManager notifManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-	Button notification;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState)
@@ -56,74 +44,6 @@ public class MainActivity extends AppCompatActivity {
 		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
-		final int NOTIFY_ID = 1002;
-		notification = (Button) findViewById(R.id.btnNotif);
-		notification.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-		// There are hardcoding only for show it's just strings
-		String name = "my_package_channel";
-		String id = "my_package_channel_1"; // The user-visible name of the channel.
-		String description = "my_package_first_channel"; // The user-visible description of the channel
 
-		Intent intent;
-		PendingIntent pendingIntent;
-		NotificationCompat.Builder builder;
-
-		if (notifManager == null) {
-			notifManager =
-							(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-		}
-
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			int importance = NotificationManager.IMPORTANCE_HIGH;
-			NotificationChannel mChannel = notifManager.getNotificationChannel(id);
-			if (mChannel == null) {
-				mChannel = new NotificationChannel(id, name, importance);
-				mChannel.setDescription(description);
-				mChannel.enableVibration(true);
-				mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-				notifManager.createNotificationChannel(mChannel);
-			}
-			builder = new NotificationCompat.Builder(getApplicationContext(), id);
-
-			intent = new Intent(MainActivity.this, MainActivity.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-			pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-
-			builder.setContentTitle("Notification")  // required
-							.setSmallIcon(android.R.drawable.ic_popup_reminder) // required
-							.setContentText("Content of notification")  // required
-							.setDefaults(Notification.DEFAULT_ALL)
-							.setAutoCancel(true)
-							.setContentIntent(pendingIntent)
-
-							.setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-		} else {
-
-			builder = new NotificationCompat.Builder(getApplicationContext(), id);
-
-			intent = new Intent(MainActivity.this, MainActivity.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-			pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-
-			builder.setContentTitle("Notification")                           // required
-							.setSmallIcon(android.R.drawable.ic_popup_reminder) // required
-							.setContentText("Content of notification")  // required
-							.setDefaults(Notification.DEFAULT_ALL)
-							.setAutoCancel(true)
-							.setContentIntent(pendingIntent)
-							.setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
-							.setPriority(Notification.PRIORITY_HIGH);
-		} // else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-		Notification notification = builder.build();
-		notifManager.notify(NOTIFY_ID, notification);
-
-
-			}
-		});
 	}
 }
