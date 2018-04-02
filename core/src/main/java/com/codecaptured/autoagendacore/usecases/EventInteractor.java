398f719f88a3cc06fb15b0cf54ba035b5ea2cbd7
Unknown <DAScott321@gmail.com>
2018-04-02 19:12:38 -0400

    UI updates

--- core/src/main/java/com/codecaptured/autoagendacore/usecases/EventInteractor.java
+++ core/src/main/java/com/codecaptured/autoagendacore/usecases/EventInteractor.java
@@ -1,147 +1,147 @@
-package com.codecaptured.autoagendacore.usecases;
-
-import com.codecaptured.autoagendacore.entities.Event;
-import com.codecaptured.autoagendacore.entities.TimeBlock;
-
-import java.util.UUID;
-import java.util.Date;
-
-/**
- * This is used when interacting (creating, modifying, removing) events. Also controls the interface
- * needed for talking to this class
- */
-public class EventInteractor
-{
-	// TODO: Should also return some sort of message to indicate if it was scheduled properly or not.
-
-	/**
-	 * Add a new event to the schedule. Will be scheduled if using the default date/time
-	 * @param newEvent The new event
-	 */
-	public static void addEvent(UserEvent newEvent)
-	{
-		// Create a new ID
-		UUID id = UUID.randomUUID();
-
-		// Add ID to UserEvent
-		newEvent.setId(id);
-
-		// Make the event
-		Event event = userEventToEvent(newEvent);
-
-		// Add to scheduler to decide where to put it in the schedule
-		Scheduler.addEvent(event);
-	}
-
-	/**
-	 * Change an existing scheduled event and reschedule it if using the default date/time
-	 * @param originalEvent The original event to be changed
-	 * @param newEvent The new changed event
-	 */
-	public static void modifyEvent(UserEvent originalEvent, UserEvent newEvent)
-	{
-		// Make the IDs the same
-		newEvent.setId(originalEvent.getId());
-
-		// Remove the old event
-		Scheduler.removeEvent(originalEvent.getId());
-
-		// Make the event
-		Event event = userEventToEvent(newEvent);
-
-		// Add to scheduler to decide where to put it in the schedule
-		Scheduler.addEvent(event);
-	}
-
-	/**
-	 * Remove an event from the schedule
-	 * @param oldEvent The event to be removed
-	 */
-	public static void removeEvent(UserEvent oldEvent)
-	{
-		// Delete old event
-		Scheduler.removeEvent(oldEvent.getId());
-	}
-
-	/**
-	 * Convert a user event to an event
-	 * @param userEvent User event to be based off of
-	 * @return event with same data as userEvent
-	 */
-	protected static Event userEventToEvent(UserEvent userEvent)
-	{
-		// Make the event
-		return new Event(userEvent.getId(), userEvent.getTitle(), userEvent.getDescription(),
-						userEvent.getEventTime(), userEvent.getPriorityLevel(), userEvent.getTags());
-	}
-
-	/**
-	 * Convert an event to a user event
-	 * @param event Event to be based off of
-	 * @param userEvent Must be passed in (interfaces can't be instantiated)
-	 * @return tempUserEvent with the same data as event
-	 */
-	protected static UserEvent eventToUserEvent(Event event, UserEvent userEvent)
-	{
-		// Set the passed user event to have all the attributes of the event
-		userEvent.setId(event.getId());
-		userEvent.setTitle(event.getTitle());
-		userEvent.setDescription(event.getDescription());
-		userEvent.setEventTime(event.getEventTime());
-		userEvent.setPriorityLevel(event.getPriorityLevel());
-		userEvent.setTags(event.getTags());
-		return userEvent;
-	}
-
-	/**
-	 * Convert multiple events to user events
-	 * @param events Events to be based off of
-	 * @param userEvents Must be passed in (interfaces can't be instantiated)
-	 * @return user events with the same data as events
-	 */
-	protected static UserEvent[] eventsToUserEvents(Event[] events, UserEvent[] userEvents)
-	{
-		// Value to be returned
-		UserEvent[] convertedEvents = new UserEvent[events.length];
-
-		// Convert each event to a user event
-		for (int i = 0; i < events.length; i++)
-		{
-			convertedEvents[i] = eventToUserEvent(events[i], userEvents[0]);
-		}
-
-		// Return new user events
-		return convertedEvents;
-	}
-
-	/**
-	 * The event data object used to talk with the use cases. Includes default values for events
-	 */
-	public interface UserEvent
-	{
-		// Default values
-		String DEFAULT_TITLE = "";
-		String DEFAULT_DESCRIPTION = "";
-		TimeBlock DEFAULT_EVENT_TIME = new TimeBlock(new Date(0), 0);
-		int DEFAULT_PRIORITY_LEVEL = 5;
-		String[] DEFAULT_TAGS = {""};
-
-		// Getters and setters
-		// Values set by user
-		String getTitle();
-		void setTitle(String title);
-		String getDescription();
-		void setDescription(String description);
-		TimeBlock getEventTime();
-		void setEventTime(TimeBlock timeBlocks);
-		int getPriorityLevel();
-		void setPriorityLevel(int priorityLevel);
-		String[] getTags();
-		void setTags(String[] tags);
-
-		// Set by software
-		UUID getId();
-		void setId(UUID id);
-	}
-
-}
+//package com.codecaptured.autoagendacore.usecases;
+//
+//import com.codecaptured.autoagendacore.entities.Event;
+//import com.codecaptured.autoagendacore.entities.TimeBlock;
+//
+//import java.util.UUID;
+//import java.util.Date;
+//
+///**
+// * This is used when interacting (creating, modifying, removing) events. Also controls the interface
+// * needed for talking to this class
+// */
+//public class EventInteractor
+//{
+//	// TODO: Should also return some sort of message to indicate if it was scheduled properly or not.
+//
+//	/**
+//	 * Add a new event to the schedule. Will be scheduled if using the default date/time
+//	 * @param newEvent The new event
+//	 */
+//	public static void addEvent(UserEvent newEvent)
+//	{
+//		// Create a new ID
+//		UUID id = UUID.randomUUID();
+//
+//		// Add ID to UserEvent
+//		newEvent.setId(id);
+//
+//		// Make the event
+//		Event event = userEventToEvent(newEvent);
+//
+//		// Add to scheduler to decide where to put it in the schedule
+//		Scheduler.addEvent(event);
+//	}
+//
+//	/**
+//	 * Change an existing scheduled event and reschedule it if using the default date/time
+//	 * @param originalEvent The original event to be changed
+//	 * @param newEvent The new changed event
+//	 */
+//	public static void modifyEvent(UserEvent originalEvent, UserEvent newEvent)
+//	{
+//		// Make the IDs the same
+//		newEvent.setId(originalEvent.getId());
+//
+//		// Remove the old event
+//		Scheduler.removeEvent(originalEvent.getId());
+//
+//		// Make the event
+//		Event event = userEventToEvent(newEvent);
+//
+//		// Add to scheduler to decide where to put it in the schedule
+//		Scheduler.addEvent(event);
+//	}
+//
+//	/**
+//	 * Remove an event from the schedule
+//	 * @param oldEvent The event to be removed
+//	 */
+//	public static void removeEvent(UserEvent oldEvent)
+//	{
+//		// Delete old event
+//		Scheduler.removeEvent(oldEvent.getId());
+//	}
+//
+//	/**
+//	 * Convert a user event to an event
+//	 * @param userEvent User event to be based off of
+//	 * @return event with same data as userEvent
+//	 */
+//	protected static Event userEventToEvent(UserEvent userEvent)
+//	{
+//		// Make the event
+//		return new Event(userEvent.getId(), userEvent.getTitle(), userEvent.getDescription(),
+//						userEvent.getEventTime(), userEvent.getPriorityLevel(), userEvent.getTags());
+//	}
+//
+//	/**
+//	 * Convert an event to a user event
+//	 * @param event Event to be based off of
+//	 * @param userEvent Must be passed in (interfaces can't be instantiated)
+//	 * @return tempUserEvent with the same data as event
+//	 */
+//	protected static UserEvent eventToUserEvent(Event event, UserEvent userEvent)
+//	{
+//		// Set the passed user event to have all the attributes of the event
+//		userEvent.setId(event.getId());
+//		userEvent.setTitle(event.getTitle());
+//		userEvent.setDescription(event.getDescription());
+//		userEvent.setEventTime(event.getEventTime());
+//		userEvent.setPriorityLevel(event.getPriorityLevel());
+//		userEvent.setTags(event.getTags());
+//		return userEvent;
+//	}
+//
+//	/**
+//	 * Convert multiple events to user events
+//	 * @param events Events to be based off of
+//	 * @param userEvents Must be passed in (interfaces can't be instantiated)
+//	 * @return user events with the same data as events
+//	 */
+//	protected static UserEvent[] eventsToUserEvents(Event[] events, UserEvent[] userEvents)
+//	{
+//		// Value to be returned
+//		UserEvent[] convertedEvents = new UserEvent[events.length];
+//
+//		// Convert each event to a user event
+//		for (int i = 0; i < events.length; i++)
+//		{
+//			convertedEvents[i] = eventToUserEvent(events[i], userEvents[0]);
+//		}
+//
+//		// Return new user events
+//		return convertedEvents;
+//	}
+//
+//	/**
+//	 * The event data object used to talk with the use cases. Includes default values for events
+//	 */
+//	public interface UserEvent
+//	{
+//		// Default values
+//		String DEFAULT_TITLE = "";
+//		String DEFAULT_DESCRIPTION = "";
+//		TimeBlock DEFAULT_EVENT_TIME = new TimeBlock(new Date(0), 0);
+//		int DEFAULT_PRIORITY_LEVEL = 5;
+//		String[] DEFAULT_TAGS = {""};
+//
+//		// Getters and setters
+//		// Values set by user
+//		String getTitle();
+//		void setTitle(String title);
+//		String getDescription();
+//		void setDescription(String description);
+//		TimeBlock getEventTime();
+//		void setEventTime(TimeBlock timeBlocks);
+//		int getPriorityLevel();
+//		void setPriorityLevel(int priorityLevel);
+//		String[] getTags();
+//		void setTags(String[] tags);
+//
+//		// Set by software
+//		UUID getId();
+//		void setId(UUID id);
+//	}
+//
+//}
